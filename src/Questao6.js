2d91e02378c267c72abf57f2e7852d22127d5ec0
marcioflorenco-code <marcio.florenco@academico.ifpb.edu.br>
2025-11-11 22:39:55 -0300

    Update Questao6.js

--- src/Questao6.js
+++ src/Questao6.js
@@ -1,1 +1,12 @@
+function parseJSONSafe(jsonString) {
+  try {
+    return JSON.parse(jsonString); 
+  } catch (erro) {
+    console.error("Erro ao converter JSON:", erro.message); 
+    return null; // Retorna null em caso de erro
+  }
+}
 
+// Teste com JSON inválido
+const jsonInvalido = '{nome: "João", "idade": 30}'; // Falta aspas em "nome"
+console.log(parseJSONSafe(jsonInvalido));

c8a7f5e290bc162e80138db23a47454c88eeed51
marcioflorenco-code <marcio.florenco@academico.ifpb.edu.br>
2025-11-11 22:21:26 -0300

    Update Questao1.js

--- src/Questao1.js
+++ src/Questao1.js
@@ -1,2 +1,12 @@
+const usuario = {
+  nome: "Ana Silva",
+  idade: 30,
+  habilidades: ["JavaScript", "React", "Node.js"],
+  ativo: true
+};
+const jsonString = JSON.stringify(usuario);//transforma em texto (formato JSON).
+console.log(jsonString);
+const usuarioRecuperado = JSON.parse(jsonString);//transforma o texto JSON de volta em objeto JavaScript.
+console.log(usuarioRecuperado);
 
 

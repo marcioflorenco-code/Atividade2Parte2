2a6045f6c14ceb8e09ed7b2da178fe3840646964
marcioflorenco-code <marcio.florenco@academico.ifpb.edu.br>
2025-11-11 22:24:00 -0300

    Update Questao2.js

--- src/Questao2.js
+++ src/Questao2.js
@@ -1,1 +1,11 @@
+const jsonString = `{
+  "usuarios": [
+    {"nome": "Carlos", "email": "carlos@email.com"},
+    {"nome": "Maria", "email": "maria@email.com"},
+    {"nome": "João", "email": "joao@email.com"}
+  ]
+}`;
 
+const dados = JSON.parse(jsonString);
+const emailsConcatenados = dados.usuarios.map(u => u.email).join(", ");
+console.log(emailsConcatenados);

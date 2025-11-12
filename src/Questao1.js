bb93a4279b63dc2c5706d15fa39fc0c1f5a6d1c2
marcioflorenco-code <marcio.florenco@academico.ifpb.edu.br>
2025-11-11 22:22:06 -0300

    Update Questao1.js

--- src/Questao1.js
+++ src/Questao1.js
@@ -8,5 +8,3 @@ const jsonString = JSON.stringify(usuario);//transforma em texto (formato JSON).
 console.log(jsonString);
 const usuarioRecuperado = JSON.parse(jsonString);//transforma o texto JSON de volta em objeto JavaScript.
 console.log(usuarioRecuperado);
-
-
